bb753612444c3017bd2aa1994e4a78d2fffcdafa
Mrázek Matěj <741-mrazekma@users.noreply.localhost>
2021-06-03 12:20:53 +0200

    Commit all files

---
+++ DaD_display.h
@@ -0,0 +1,42 @@
+#ifndef DAD_DISPLAY_H
+#define DAD_DISPLAY_H
+
+#include "segment_display.h"
+
+
+
+constexpr uint8_t dice_types[][2]{{4, DIGIT_NONE}, {6, DIGIT_NONE}, {8, DIGIT_NONE}, {1, 0}, {1, 2}, {2, 0}, {0, 0}};
+constexpr uint8_t dice_sizes[]{4, 6, 8, 10, 12, 20, 100};
+constexpr uint8_t dice_count = 7;
+
+
+class DiceSegmentDisplay : public NumberSegmentDisplay{
+  uint8_t throw_count;
+  uint8_t dice_i;
+public:
+  DiceSegmentDisplay() : throw_count(3), dice_i(2){
+    updateConfigState();
+  }
+  void incrThrowCount(){
+    throw_count = throw_count % 9 + 1;
+    updateConfigState();
+  }
+  void incrDiceType(){
+    dice_i = (dice_i + 1) % dice_count;
+    updateConfigState();
+  }
+  void generateNumber(){
+    uint16_t n = 0;
+    for (int i = 0; i < throw_count; i++){
+      n += random(dice_sizes[dice_i]) + 1;
+    }
+    setNumber(n);
+  }
+private:
+  void updateConfigState(){
+    setSegments(throw_count, DIGIT_d, dice_types[dice_i][0], dice_types[dice_i][1]);
+  }
+};
+
+
+#endif
---
+++ solution_fixed.ino
@@ -0,0 +1,146 @@
+// (:]) @tom: super, moc pekny OOP a pouzviti C++
+
+#inlude "funshield.h"
+
+
+
+class Stopwatch{
+  uint16_t timer_start;
+public:
+  Stopwatch()
+  {}
+  void init(){
+    reset();
+  }
+  inline uint16_t elapsedTime(){
+    return millis() - timer_start;
+  }
+  inline void reset(){
+    timer_start = millis();
+  }
+};
+
+
+class Timer : public Stopwatch{
+  // (FIXED) @tom: tohle se nemeni, muze to byt jako cokoliv jineho `const`
+  const uint16_t timer_length;
+public:
+  Timer(uint16_t length = 0) : timer_length(length)
+  {}
+  bool update(){
+    if (elapsed()){
+      reset();
+      return true;
+    }
+    return false;
+  }
+  bool elapsed(){
+    return elapsedTime() >= timer_length;
+  }
+};
+
+class Button{
+  const uint8_t pin;
+public:
+  Button(uint8_t p) : pin(p)
+  {}
+  void init(){
+    pinMode(pin, INPUT);
+  }
+  bool isPressed(){
+    return !digitalRead(pin);
+  }
+};
+
+class DoublyTimedButton : public Button{
+  // (FIXED) @tom: idealne pouzivat enum class -> je to bezpecnejsi
+  // Bezpecnejsi to je urcite, kdyz je enum global, ale kdyz je uvnitr tridy, tak je to celkem jedno, ne? Beztak pujdou ty promenne videt jenom v te tride (jsou private), a zjednodussi mi to kod.
+  enum class PressState{
+    NOT_PRESSED,
+    PRESSED_FIRST,
+    PRESSED_LATER
+  } state = PressState::NOT_PRESSED;
+  Timer timer_first;
+  Timer timer_later;
+public:
+  DoublyTimedButton(uint8_t pin, uint16_t cd_first, uint16_t cd) : Button(pin), timer_first(cd_first), timer_later(cd)
+  {}
+  void init(){
+    Button::init();
+    timer_first.init();
+    timer_later.init();
+  }
+  bool isPressed(){
+    if (Button::isPressed()){
+      if (state == PressState::NOT_PRESSED){
+        timer_first.reset();
+        state = PressState::PRESSED_FIRST;
+        return true;
+      }else if (state == PressState::PRESSED_FIRST && timer_first.elapsed()){
+        timer_later.reset();
+        state = PressState::PRESSED_LATER;
+        return true;
+      }else if (state == PressState::PRESSED_LATER && timer_later.elapsed()){
+        timer_later.reset();
+        return true;
+      }
+      return false;
+    }else{
+      state = PressState::NOT_PRESSED;
+      return false;
+    }
+  }
+};
+
+
+class LED{
+  const uint8_t pin;
+public:
+  LED(uint8_t p) : pin(p)
+  {}
+  void init(){
+    pinMode(pin, OUTPUT);
+  }
+  void turnOn() const{
+    digitalWrite(pin, ON);
+  }
+  void turnOff() const{
+    digitalWrite(pin, OFF);
+  }
+};
+
+const LED LEDs[]{LED(led1_pin), LED(led2_pin), LED(led3_pin), LED(led4_pin)};
+constexpr uint8_t LED_count = sizeof(LEDs) / sizeof(uint8_t);
+
+DoublyTimedButton b_incr{button1_pin, 1000, 300};
+DoublyTimedButton b_subt{button2_pin, 1000, 300};
+
+uint8_t counter = 0;
+const uint8_t counter_max_n = 16;
+
+
+void setup(){
+  for (LED l : LEDs){
+    l.init();
+  }
+  b_incr.init();
+  b_subt.init();
+}
+
+
+
+void loop() {
+  if (b_incr.isPressed()){
+    counter = (counter + 1) % counter_max_n;
+  }
+  if (b_subt.isPressed()){
+    counter = (counter + counter_max_n - 1) % counter_max_n; // (FIXED) @tom: jeste pojmenovat konstanty a je to top
+  }
+  for (int i = 0; i < LED_count; i++){
+    if (counter & (8 >> i)){
+      LEDs[i].turnOn();
+    }else{
+      LEDs[i].turnOff();
+    }
+  }
+}
